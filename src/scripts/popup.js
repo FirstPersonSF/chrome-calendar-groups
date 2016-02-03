11b7983d4334cd7a88debf76350e5590df0b7f5f
Chris <christrinh@firstperson.is>
2016-02-02 17:36:31 -0800

    Fix id grapping

--- src/scripts/popup.js
+++ src/scripts/popup.js
@@ -263,7 +263,7 @@ popupAction.loadInputSelection = function() {
     // Create set
     createSet.find('.btn').click(function(e){
       // Create object id
-      var setId = createSet.find('.main-title').data('id');
+      var setId = createSet.find('.main-title').attr('data-id');
       var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
         var r = crypto.getRandomValues(new Uint8Array(1))[0]%16|0, v = c == 'x' ? r : (r&0x3|0x8);
         return v.toString(16);
