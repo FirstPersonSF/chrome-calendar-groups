97eea3b511af930e8519f0d8b0fdbb3a877a3d95
Chris <christrinh@firstperson.is>
2016-02-03 14:19:52 -0800

    Add create by first person on footer and new icons.

--- src/scripts/popup.js
+++ src/scripts/popup.js
@@ -15,6 +15,7 @@ popupAction.settimeout = {}
  * @const
  */
 popupAction.elem = {
+  header: 'main-header',
   footer: 'main-footer',
   auth: 'error-auth',
   mainContent : 'main-content',
@@ -83,7 +84,7 @@ popupAction.installButtonClickHandlers = function() {
 
   // Add new set
   actionBar.find('.btn-add-group').click(function(){
-    createSet.find('.main-title').text('Add Groups').attr({'data-id':''});
+    createSet.find('.main-title').text('Add Group').attr({'data-id':''});
     createSet.find('.btn').text('Create');
     popupAction.resetDisplay();
     popupAction.addonAddSet();
@@ -96,8 +97,7 @@ popupAction.installButtonClickHandlers = function() {
       popupAction.globalDad.activate();
     }else{
       $(this).text('Edit').css({color:''});
-      popupAction.globalDad.deactivate();
-      popupAction.putSetsOrder();
+      popupAction.putSetsOrder(popupAction.globalDad.deactivate);
     }
   });
 
@@ -116,8 +116,7 @@ popupAction.installButtonClickHandlers = function() {
 popupAction.resetDisplay = function(){
   var setLists = $('#' + popupAction.elem.setLists);
   setLists.find('.btn-order-list').text('Edit').css({color:''});
-  popupAction.globalDad.deactivate();
-  popupAction.putSetsOrder();
+  popupAction.putSetsOrder(popupAction.globalDad.deactivate);
 }
 
 /**
@@ -220,17 +219,17 @@ popupAction.displaySetLists = function(){
 
     // Delete Set
     setLists.find('.list .btn-delete').click(function(){
-      popupAction.deleteSet($(this).closest('.item').data('id'));
+      popupAction.deleteSet($(this).closest('.item').attr('data-id'));
     });
 
     // Edit Set
     setLists.find('.list .btn-edit').click(function(){
-      popupAction.addonAddSet(setsStorage[$(this).closest('.item').data('id')]);
+      popupAction.addonAddSet(setsStorage[$(this).closest('.item').attr('data-id')]);
     });
 
     // Update Calendar selection list
     setLists.find('.btn-change-calendar').click(function(){
-      var setId = $(this).closest('.item').data('id');
+      var setId = $(this).closest('.item').attr('data-id');
       if(!$(this).hasClass('dad-active')){
         chrome.extension.sendMessage({method: 'selection.sets.disabled'});
         _.each(setsStorage, function(obj){(obj.id == setId)? obj.selected = true : obj.selected = false;});
@@ -333,14 +332,16 @@ popupAction.deleteSet = function(setId){
  * Retrieve Sets object form Local storage and save the new
  * order that that was assign to set. Update View after save.
  */
-popupAction.putSetsOrder = function(){
-  storage.local.getSets(function(setsStorage){
-    var setLists = $('#' + popupAction.elem.setLists + ' .list' );
+popupAction.putSetsOrder = function(callback){
+  var setLists = $('#' + popupAction.elem.setLists + ' .list' );
+  if(!setLists.hasClass('dad-active')) return;
 
+  storage.local.getSets(function(setsStorage){
     setLists.find('.item').each(function(){
-      setsStorage[$(this).data('id')].order = $(this).data('dad-position');
+      if($(this).attr('data-dad-position')) setsStorage[$(this).attr('data-id')].order = $(this).attr('data-dad-position');
     });
 
+    if(callback) callback();
     storage.local.putSets(setsStorage, popupAction.displaySetLists);
   });
 };
